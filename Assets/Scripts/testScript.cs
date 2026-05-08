fa87712df1d076a3f33e67ca49a9abe8a5f2887c
Alba Damián <albadmmm@gmail.com>
2026-05-08 12:00:46 +0200

    segunda prueba

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -5,7 +5,7 @@ public class testScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //hjghjhg
+        //hjghjhg kjh kjh kjh kjh kjh kjh 
     }
 
     // Update is called once per frame
