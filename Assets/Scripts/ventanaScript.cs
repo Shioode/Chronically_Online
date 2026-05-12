4027728280120784b80faf440b35ac1df7ccb4a6
Desirée Beltrán <desicabanes@gmail.com>
2026-05-12 13:36:07 +0200

    Animado y ajustado ventana de spam

--- Assets/Scripts/ventanaScript.cs
+++ Assets/Scripts/ventanaScript.cs
@@ -1,17 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ventanaScript : MonoBehaviour
 {
-    public GameObject Ventana;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() { }
 
     // Update is called once per frame
     void Update() { }
 
-    void Delete()
+    public void OnMouseClick()
     {
-        
+        gameObject.SetActive(false);
+
+        // gameObject.GetComponent<Animator>().SetBool("obtenerCoin", true);
     }
 }
