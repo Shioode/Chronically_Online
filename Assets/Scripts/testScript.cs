4764d6e36de5418da329ab56c2efe5244a2404b8
Shioode <desicabanes@gmail.com>
2026-05-13 13:44:33 +0200

    intento de merge de la rama desi a la main

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class testScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+<<<<<<< HEAD
+=======
+    void Start()
+    {
+        //hjghjhg kjh kjh kjh kjh kjh kjh 
+    }
+>>>>>>> origin/main
+
 
     // Update is called once per frame
     void Update() { }
