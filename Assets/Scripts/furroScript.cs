1e400e9e3c7a57b58978ab88a769176e25a00710
Alba Romero <aidromero04@gmail.com>
2026-05-15 20:36:51 +0200

    game manager spam limpio nuevo game manager furro

--- Assets/Scripts/furroScript.cs
+++ Assets/Scripts/furroScript.cs
@@ -2,22 +2,11 @@ using UnityEngine;
 
 public class furroScript : MonoBehaviour
 {
-    /// <summary>
-    /// [SerializeField]
-    /// </summary>
-    //private float cantidadPuntos;
-
-    //[SerializeField]
-    //private Puntaje puntosGatita;
-
-    //public GameObject ObjPuntos;
     public int valor = 1;
 
     GameObject Gatita;
     GameObject Zorrona;
     GameObject Caballito;
-
-    //Fpublic static int puntosGatita;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,108 +19,40 @@ public class furroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Gatita.SetActive(true);
-        //Zorrona.SetActive(false);
-        //Caballito.SetActive(false);
-
-        if (gameManagerSpam.puntos < 3)
+        if (gameManagerFurros.puntos < 3)
         {
             Gatita.SetActive(true);
             Zorrona.SetActive(false);
             Caballito.SetActive(false);
         }
-
-        /*if (gameManagerSpam.puntos >= 3)
-        {
-            Gatita.SetActive(false);
-            Zorrona.SetActive(true);
-            Caballito.SetActive(false);
-        }
-
-        if (gameManagerSpam.puntos >= 3 && gameManagerSpam.puntosZorrona >= 3)
-        {
-            Gatita.SetActive(false);
-            Zorrona.SetActive(false);
-            Caballito.SetActive(true);
-        }
-
-        //if (Zorrona == false && Caballito == false)
-        //{
-        //    Gatita.SetActive(true);
-        //Caballito.SetActive(false);
-        //}
-
-        //if (gameManagerSpam.puntosGatita >= 3)
-        //{
-        //    Gatita.SetActive(false);
-        //}
-
-        //if (Gatita == false && Caballito == false)
-        //{
-        //   Zorrona.SetActive(true);
-        //Caballito.SetActive(false);
-        //}
-
-        //if (gameManagerSpam.puntosZorrona >= 3)
-        //{
-        //    Zorrona.SetActive(false);
-        //}
-
-        //if (Gatita == false && Zorrona == false)
-        //{
-        //    Caballito.SetActive(true);
-        //}
-
-        //if (gameManagerSpam.puntosCaballito >= 3)
-        //{
-        //    Caballito.SetActive(false);
-        //}
-        */
     }
 
     public void OnMouseClick1()
     {
         gameObject.SetActive(false);
-        gameManagerSpam.puntos += valor;
-        Debug.Log("puntos =" + gameManagerSpam.puntos);
+        gameManagerFurros.puntos += valor;
+        Debug.Log("puntos =" + gameManagerFurros.puntos);
         //Puntaje.SumarPuntos(cantidadPuntos);
 
-        if (gameManagerSpam.puntos >= 3)
+        if (gameManagerFurros.puntos >= 3)
         {
             Gatita.SetActive(false);
             Zorrona.SetActive(true);
             Caballito.SetActive(false);
         }
 
-        if (gameManagerSpam.puntos >= 6)
+        if (gameManagerFurros.puntos >= 6)
         {
             Gatita.SetActive(false);
             Zorrona.SetActive(false);
             Caballito.SetActive(true);
         }
 
-        if (gameManagerSpam.puntos >= 9)
+        if (gameManagerFurros.puntos >= 9)
         {
             Gatita.SetActive(false);
             Zorrona.SetActive(false);
             Caballito.SetActive(false);
         }
     }
-
-    //public void OnMouseClick2()
-    //{
-    //.puntosZorrona += cantidadPuntos;
-    //    gameObject.SetActive(false);
-    //}
-
-    //public void OnMouseClick3()
-    //{
-    //    gameObject.SetActive(false);
-    //puntosCaballito += cantidadPuntos;
-
-    //    if (gameManagerSpam.puntosCaballito >= 3)
-    //    {
-    //        Caballito.SetActive(false);
-    //}
-    //}
 }
--- Assets/Scripts/gameManagerSpam.cs
+++ Assets/Scripts/gameManagerSpam.cs
@@ -3,48 +3,9 @@ using UnityEngine;
 
 public class gameManagerSpam : MonoBehaviour
 {
-    public static int puntos = 0;
-
-    GameObject puntaje;
-
-    //public static int puntosZorrona = 0;
-    //public static int puntosCaballito = 0;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        puntaje = GameObject.Find("puntaje");
-        //Debug.Log("puntosGatita");
-        //Debug.Log("puntosZorrona");
-        //Debug.Log("puntosCaballito");
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        puntaje.GetComponent<TextMeshProUGUI>().text = puntos.ToString();
-    }
-
-    //void Panel()
-    //{
-    //    if (puntosGatita >= 3)
-    //   {
-    //      Gatita.SetActive(false);
-    //          Zorrona.SetActive(true);
-    //    }
-
-    //  if (puntosZorrona >= 3)
-    //{
-    //  Gatita.SetActive(false);
-    //Zorrona.SetActive(false);
-    //Caballito.SetActive(true);
-    //}
-
-    //if (puntosCaballito >= 3)
-    //{
-    //    Gatita.SetActive(false);
-    //   Zorrona.SetActive(false);
-    //    Caballito.SetActive(false);
-    //   }
-    //}
+    void Update() { }
 }
