4a3a32ca861124d5dab94fec7021d3418562a450
Alba Romero <aidromero04@gmail.com>
2026-05-16 22:19:10 +0200

    Revert "Alba2"

--- Assets/Scripts/furroScript.cs
+++ Assets/Scripts/furroScript.cs
@@ -2,57 +2,9 @@ using UnityEngine;
 
 public class furroScript : MonoBehaviour
 {
-    public int valor = 1;
-
-    GameObject Gatita;
-    GameObject Zorrona;
-    GameObject Caballito;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Gatita = GameObject.Find("Fondo1");
-        Zorrona = GameObject.Find("Fondo2");
-        Caballito = GameObject.Find("Fondo3");
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (gameManagerFurros.puntos < 3)
-        {
-            Gatita.SetActive(true);
-            Zorrona.SetActive(false);
-            Caballito.SetActive(false);
-        }
-    }
-
-    public void OnMouseClick1()
-    {
-        gameObject.SetActive(false);
-        gameManagerFurros.puntos += valor;
-        Debug.Log("puntos =" + gameManagerFurros.puntos);
-        //Puntaje.SumarPuntos(cantidadPuntos);
-
-        if (gameManagerFurros.puntos >= 3)
-        {
-            Gatita.SetActive(false);
-            Zorrona.SetActive(true);
-            Caballito.SetActive(false);
-        }
-
-        if (gameManagerFurros.puntos >= 6)
-        {
-            Gatita.SetActive(false);
-            Zorrona.SetActive(false);
-            Caballito.SetActive(true);
-        }
-
-        if (gameManagerFurros.puntos >= 9)
-        {
-            Gatita.SetActive(false);
-            Zorrona.SetActive(false);
-            Caballito.SetActive(false);
-        }
-    }
+    void Update() { }
 }
--- Assets/Scripts/gameManagerSpam.cs
+++ Assets/Scripts/gameManagerSpam.cs
@@ -1,11 +1,16 @@
-using TMPro;
 using UnityEngine;
 
 public class gameManagerSpam : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        
+    }
 }
