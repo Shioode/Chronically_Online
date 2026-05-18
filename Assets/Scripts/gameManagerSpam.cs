53f6501c06f77c5dd914059d267497e2f74dcd5d
Desirée Beltrán <desicabanes@gmail.com>
2026-05-18 20:34:01 +0200

    Puntuacion añadida

--- Assets/Scripts/gameManagerSpam.cs
+++ Assets/Scripts/gameManagerSpam.cs
@@ -10,7 +10,7 @@ public class gameManagerSpam : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        puntuacion = GameObject.Find("Puntuacion");
+        puntuacion = GameObject.Find("puntuacion_Text");
     }
 
     // Update is called once per frame
